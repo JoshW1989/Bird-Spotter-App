e27e73b015caaf4ec403fe59f1922791bd0b3eee
Aidan Harris <B00361851@studentmail.uws.ac.uk>
2020-04-04 16:57:45 +0100

    Signed-off-by: Aidan Harris <B00361851@studentmail.uws.ac.uk>

--- app/src/main/java/com/example/birdspotter/AddActivity.java
+++ app/src/main/java/com/example/birdspotter/AddActivity.java
@@ -36,6 +36,8 @@ public class AddActivity extends AppCompatActivity {
     public  String   submitName;
 
 
+    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
